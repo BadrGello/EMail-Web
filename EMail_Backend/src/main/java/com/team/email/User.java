7ba350a1ac73d77b7c18cf08d3a4df9ef4dbc383
Ali Eldeen <aliedeen17@gmail.com>
2024-12-09 22:41:56 +0200

    .

--- EMail_Backend/src/main/java/com/team/email/User.java
+++ EMail_Backend/src/main/java/com/team/email/User.java
@@ -1,4 +1,13 @@
 package com.team.email;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class User implements UserInterface {
     private String id;
@@ -7,9 +16,13 @@ public class User implements UserInterface {
     private String email;
     private Contacts contacts;
     private MailFolders mailFolders;
-    // More to be added when Mail created
 
-    User (UserBuilder builder){
+
+
+    public User() {
+    }
+   
+    private User (UserBuilder builder){
         this.id = builder.id;
         this.userName = builder.userName;
         this.password = builder.password;
@@ -17,6 +30,37 @@ public class User implements UserInterface {
         this.contacts = builder.contacts;
         this.mailFolders = builder.mailFolders;
 
+    }
+     public void save(String filePath) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File jsonFile = new File(filePath);
+        objectMapper.writeValue(jsonFile, this);
+        validateSchema(jsonFile);
+
+        System.out.println("User saved");
+    }
+
+ 
+    public User load(String filePath) throws Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        File jsonFile = new File(filePath);
+        validateSchema(jsonFile); 
+        return objectMapper.readValue(jsonFile, User.class);
+    }
+
+
+    private static void validateSchema(File jsonFile) throws Exception {
+
+        FileInputStream schemaStream = new FileInputStream(SchemaConfig.SCHEMA_PATH);
+        JSONObject schemaJSON = new JSONObject(new JSONTokener(schemaStream));
+        Schema schema = SchemaLoader.load(schemaJSON);
+
+        FileInputStream jsonStream = new FileInputStream(jsonFile);
+        JSONObject jsonObject = new JSONObject(new JSONTokener(jsonStream));
+
+     
+        schema.validate(jsonObject); 
     }
     public static class UserBuilder{
     private String id;
@@ -39,8 +83,16 @@ public class User implements UserInterface {
         this.id = id;
         return this;    
     }
+    public UserBuilder setMailFolders(MailFolders mailFolders){
+        this.mailFolders= mailFolders;
+        return this;
+    }
+    public User build(){
+        User user = new User(this);
+        return user;
     }
 
+    }
 
 
 }
